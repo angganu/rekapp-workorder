9d2f31a0079b737c34fda39248cb48ddb99087b9
AnggaNu <annu.nura@gmail.com>
2025-02-19 16:29:30 +0700

    Fix

--- api/database/migrations/2025_02_19_144320_create_wo_requests_table.php
+++ api/database/migrations/2025_02_19_144320_create_wo_requests_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->unsignedBigInteger('area_id');
             $table->unsignedBigInteger('room_id');
             $table->unsignedBigInteger('asset_id');
-            $table->unsignedBigInteger('location_id');
 
             $table->string('code', 100)->unique();
             $table->string('name');
